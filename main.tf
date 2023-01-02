d9f8bc9dbfbd5626d6ed651183e8c1b10e07ad61
Tiago Sabadini <sabadini.tiago@gmail.com>
2023-01-02 08:07:55 -0300

    First commit

--- main.tf
+++ main.tf
@@ -1,44 +1,42 @@
 provider "google" {
-  project = "barbero-devops-iac"
+  project = "itgolabs-devops-prod"
   region  = "us-central1"
   zone    = "us-central1-c"
   credentials = "${file("serviceaccount.yaml")}"
 }
 
-resource "google_folder" "Financeiro" {
-  display_name = "Financeiro"
-  parent       = "organizations/540829645030"
+resource "google_folder" "Devops2" {
+  display_name = "Devops2"
+  parent       = "organizations/80030572804?"
 }
 
-resource "google_folder" "SalesForce" {
-  display_name = "SalesForce"
-  parent       = google_folder.Financeiro.name
+resource "google_folder" "RentalCompanies" {
+  display_name = "RentalCompanies"
+  parent       = google_folder.Devops2.name
 }
 
-resource "google_folder" "Desenvolvimento" {
-  display_name = "Desenvolvimento"
-  parent       = google_folder.SalesForce.name
+resource "google_folder" "Localiza" {
+  display_name = "Localiza"
+  parent       = google_folder.RentalCompanies.name
 }
 
-resource "google_folder" "Producao" {
-  display_name = "Producao"
-  parent       = google_folder.SalesForce.name
+resource "google_folder" "Movida" {
+  display_name = "Movida"
+  parent       = google_folder.RentalCompanies.name
 }
 
-
-resource "google_project" "barberosa2-salesforce-dev" {
-  name       = "SalesForce-Dev"
-  project_id = "barberosa2-salesforce-dev"
-  folder_id  = google_folder.Desenvolvimento.name
+resource "google_project" "itgolabs-localiza-prod" {
+  name       = "itgolabs-localiza-prod"
+  project_id = "itgolabs-localiza-prod"
+  folder_id  = google_folder.Localiza.name
   auto_create_network=false
-  billing_account = "018973-A8340F-83D8E5"
-
+  billing_account = "01259F-85B956-21408?"
 }
 
-# resource "google_project" "barberosa-salesforce-prod" {
-#   name       = "SalesForce-Prod"
-#   project_id = "barberosa-salesforce-prod"
-#   folder_id  = google_folder.Producao.name
-#   auto_create_network=false
-#   billing_account = "01B2C4-70459E-F53446"
-# }
+resource "google_project" "itgolabs-movida-prod" {
+  name       = "itgolabs-movida-prod"
+  project_id = "itgolabs-movida-prod"
+  folder_id  = google_folder.Movida.name
+  auto_create_network=false
+  billing_account = "01259F-85B956-21408?"
+}
